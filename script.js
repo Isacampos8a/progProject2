f8da0e56ae5cf704daac27548fa5a34029a0677e
Isabel Campos <isacampos8a@gmail.com>
2025-02-01 16:01:33 -0600

    fixed some of the script

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ function startTimer() {
     countdownInterval = setInterval(() => {
         const minutesLeft = Math.floor(currentTime / 60);
         const secondsLeft = currentTime % 60;
-        console.log(`Countdown: {minutesLeft}min {secondsLeft}sec`);
+        document.getElementById("timerDisplay").textContent = `Time Left: ${minutesLeft} min ${secondsLeft} sec`
 
         if (currentTime <= 0) {
             clearTnterval(countdownInterval);
             alert("Time's Up! It has reached peak egg-cellence")
-        }
+            document.getElementById("timerDisplay").textContent = `Times's Up!`
 
         currentTime--;
-    },1000);
+    },1000); //updates every second
 
 
 }
