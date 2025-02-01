e27dc7722ccef80cccf131aca03ebad1772dc02a
Isabel Campos <isacampos8a@gmail.com>
2025-01-31 21:25:24 -0600

    fixed html

---
+++ script.js
@@ -0,0 +1,29 @@
+let countdownInterval;
+
+function startTimer() {
+    const minutes = document.getElementById("inputTimer").value;
+    const targetTime = minutes * 60;
+    let currentTime = targetTime;
+
+
+
+    //clears the countdown
+    if (countdownInterval){
+        clearInterval(countdownInterval);
+    }
+
+    countdownInterval = setInterval(); {
+        const minutesLeft = Math.floor(currentTime / 60);
+        const secondsLeft = currentTime % 60;
+        console.log("Countdown: {minutesLeft}min {secondsLeft}sec");
+
+        if (currentTime <= 0) {
+            clearTnterval(countdownInterval);
+            alert("Time's Up! It has reached peak egg-cellence")
+        }
+
+        currentTime--;
+    }
+
+
+}
